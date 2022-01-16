4f96ff77c9f5d2233935c98b65b9f6d5a83353b7
SpencerTangTMT <97115538+SpencerTangTMT@users.noreply.github.com>
2022-01-16 16:00:54 +0700

    Update Cyclistic.sql

--- Cyclistic.sql
+++ Cyclistic.sql
@@ -4,7 +4,7 @@ Data Analyst: Spencer Tang
 Goal: How do annual members and casual riders use Cyclistic bikes differently?
 */
 
---After importing 12 files (12 months divvy-tripdata), creating empty table trip_12_month with the original table structure
+-- After importing 12 files (12 months divvy-tripdata), creating empty table trip_12_month with the original table structure
 CREATE TABLE [dbo].[trip_12_month](
 	[ride_id] [varchar](50) NULL,
 	[rideable_type] [varchar](50) NULL,
@@ -21,7 +21,7 @@ CREATE TABLE [dbo].[trip_12_month](
 	[member_casual] [varchar](50) NULL
 );
 
---Combining all tables into one (between 10.2020 and 09.2021)
+-- Combining all tables into one (between 10.2020 and 09.2021)
 INSERT INTO [dbo].[trip_12_month] SELECT * FROM dbo.[202010-divvy-tripdata];
 INSERT INTO [dbo].[trip_12_month] SELECT * FROM dbo.[202011-divvy-tripdata];
 INSERT INTO [dbo].[trip_12_month] SELECT * FROM dbo.[202012-divvy-tripdata];
@@ -35,3 +35,4 @@ INSERT INTO [dbo].[trip_12_month] SELECT * FROM dbo.[202107-divvy-tripdata];
 INSERT INTO [dbo].[trip_12_month] SELECT * FROM dbo.[202108-divvy-tripdata];
 INSERT INTO [dbo].[trip_12_month] SELECT * FROM dbo.[202109-divvy-tripdata];
 
+-- 
