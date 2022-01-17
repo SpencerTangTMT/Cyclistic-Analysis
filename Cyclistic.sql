3fb7a05a5423ded872ccb91f17f54d2c74c3372f
SpencerTangTMT <97115538+SpencerTangTMT@users.noreply.github.com>
2022-01-17 21:26:28 +0700

    Update Cyclistic.sql

--- Cyclistic.sql
+++ Cyclistic.sql
@@ -56,6 +56,26 @@ INSERT INTO [dbo].[trip_12_month] SELECT * FROM dbo.[202109-divvy-tripdata];
 		[member_casual] NOT LIKE '%NULL%
 );
 
+-- Removing null values from the combined dataset
+[dbo].[trip_12_month_not_null] AS
+(
+	SELECT *
+	FROM [dbo].[trip_12_month]
+	WHERE
+		[ride_id] NOT LIKE '%NULL%,
+		[rideable_type] NOT LIKE '%NULL%,
+		[started_at] NOT LIKE '%NULL%,
+		[ended_at] NOT LIKE '%NULL%,
+		[start_station_name] NOT LIKE '%NULL%,
+		[start_station_id] NOT LIKE '%NULL%,
+		[end_station_name] NOT LIKE '%NULL%,
+		[end_station_id] NOT LIKE '%NULL%,
+		[start_lat] NOT LIKE '%NULL%,
+		[start_lng] NOT LIKE '%NULL%,
+		[end_lat] NOT LIKE '%NULL%,
+		[end_lng] NOT LIKE '%NULL%,
+		[member_casual] NOT LIKE '%NULL%
+);
 -- Removing the duplication of ride id
 WITH cte
      AS (SELECT ROW_NUMBER() OVER (PARTITION BY [ride_id]
